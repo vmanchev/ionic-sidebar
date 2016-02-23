3c87068ebfca5977dc0c7b4e60d9220fc47f8dd2
Venelin Manchev <vmanchev@titlextechnology.com>
2016-02-23 17:56:26 +0200

    updated version and removed cache js code

--- www/js/app.js
+++ www/js/app.js
@@ -8,8 +8,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-
-        window.cache.clear(success);      
       
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
