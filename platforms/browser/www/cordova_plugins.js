9e730b68117bb1cb61031930881f325d5675436c
Venelin Manchev <vmanchev@titlextechnology.com>
2016-02-23 18:18:35 +0200

    updated android, config and version

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,19 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.plugin.cache/www/Cache.js",
-        "id": "org.apache.cordova.plugin.cache.Cache",
-        "pluginId": "org.apache.cordova.plugin.cache",
-        "clobbers": [
-            "cache"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2-dev",
-    "org.apache.cordova.plugin.cache": "1.0.5"
+    "cordova-plugin-whitelist": "1.2.2-dev"
 }
 // BOTTOM OF METADATA
 });
