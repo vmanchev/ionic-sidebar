2b9b287098f7519132b7b976f66dc703efc57e6e
Venelin Manchev <vmanchev@titlextechnology.com>
2016-02-23 09:37:52 +0200

    no whitelist

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,11 +1,5 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "runs": true
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {}
